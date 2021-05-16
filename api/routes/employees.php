9ae91e063a888b535bbd950d8a341ec1789686e0
KospoAmela <kospoamela1@gmail.com>
2021-05-16 14:24:38 +0200

    register route fixed

--- api/routes/employees.php
+++ api/routes/employees.php
@@ -2,21 +2,22 @@
 /**
  * @OA\Info(title="SE_PROJECT API", version="1.0")
  * @OA\OpenApi(
- *    @OA\Server(url="http://localhost:8082/SE_PROJECT/api/", description="Development Environment" ),
+ *    @OA\Server(url="http://localhost/SE_PROJECT/api/", description="Development Environment" ),
  *    @OA\Server(url="https://", description="Production Environment" )
  * ),
  * @OA\SecurityScheme(securityScheme="ApiKeyAuth", type="apiKey", in="header", name="Authentication" )
  */
 
 /**
- * @OA\Post(path="/register", tags={"login"},
+ * @OA\Post(path="/register", tags={"register"},
  *   @OA\RequestBody(description="Basic user info", required=true,
  *       @OA\MediaType(mediaType="application/json",
  *    			@OA\Schema(
- *    				 @OA\Property(property="account", required="true", type="string", example="My Test Account",	description="Name of the account" ),
- *     				 @OA\Property(property="name", required="true", type="string", example="First Last Name",	description="Name of the user`" ),
+ *     				 @OA\Property(property="name", required="true", type="string", example="First Name",	description="Name of the user`" ),
+ *             @OA\Property(property="surname", required="true", type="string", example="Surname Name",	description="Surname of the user`" ),
  *    				 @OA\Property(property="email", required="true", type="string", example="myemail@gmail.com",	description="User's email address" ),
- *             @OA\Property(property="password", required="true", type="string", example="12345",	description="Password" )
+ *             @OA\Property(property="password", required="true", type="string", example="12345",	description="Password" ),
+ *             @OA\Property(property="phone", required="true", type="string", example="12345",	description="Phone number" )
  *          )
  *       )
  *     ),
@@ -34,7 +35,7 @@ Flight::route('POST /register', function(){
  *       @OA\MediaType(mediaType="application/json",
  *    			@OA\Schema(
  *    				 @OA\Property(property="email", required="true", type="string", example="myemail@gmail.com",	description="User's email address" ),
- *             @OA\Property(property="password", required="true", type="string", example="12345",	description="Password" )
+ *                   @OA\Property(property="password", required="true", type="string", example="12345",	description="Password" )
  *          )
  *       )
  *     ),
